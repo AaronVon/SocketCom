40c890edf84785487c96cdba69a697bc2d7c9af2
Aaron <pioneervon@gmail.com>
2016-01-20 20:11:11 +0800

    Server of SocketCom
    Signed-off-by: Aaron <pioneervon@gmail.com>

--- app/src/main/java/com/pioneer/aaron/socketcom/MainActivity.java
+++ app/src/main/java/com/pioneer/aaron/socketcom/MainActivity.java
@@ -2,12 +2,157 @@ package com.pioneer.aaron.socketcom;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketException;
+import java.util.Enumeration;
+import java.util.Iterator;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+    @Bind(R.id.info_port)
+    TextView info_port;
+    @Bind(R.id.info_ip)
+    TextView info_ip;
+    @Bind(R.id.msg)
+    TextView msg;
+
+    String message = "";
+    ServerSocket serverSocket;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        
+        loadView();
+    }
+
+    private void loadView() {
+        info_ip.setText(getIPAddress());
+
+        Thread socketServerThread = new Thread(new SocketServerThread());
+        socketServerThread.start();
+    }
+
+    private String getIPAddress() {
+        String ip = "";
+        try {
+            Enumeration<NetworkInterface> enumeration = NetworkInterface.getNetworkInterfaces();
+            while (enumeration.hasMoreElements()) {
+                NetworkInterface networkInterface = enumeration.nextElement();
+                Enumeration<InetAddress> enumInetAddress = networkInterface.getInetAddresses();
+                while (enumInetAddress.hasMoreElements()) {
+                    InetAddress inetAddress = enumInetAddress.nextElement();
+                    if (inetAddress.isSiteLocalAddress()) {
+                        ip += "siteLocalAddress: " + inetAddress.getHostAddress() + "\n";
+                    }
+                }
+            }
+        } catch (SocketException e) {
+            e.printStackTrace();
+            ip += "Something Wrong!\n" + e.toString() + "\n";
+        }
+
+        return ip;
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (serverSocket != null) {
+            try {
+                serverSocket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private class SocketServerThread extends Thread {
+        static final int SocketServerPort = 8888;
+        int count = 0;
+
+        @Override
+        public void run() {
+            try {
+                serverSocket = new ServerSocket(SocketServerPort);
+                MainActivity.this.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        info_port.setText("Listening: " + serverSocket.getLocalPort());
+                    }
+                });
+
+                while (true) {
+                    Socket socket = serverSocket.accept();
+                    ++count;
+                    message += "#" + count + " from " + socket.getInetAddress() + ":" + socket.getPort() + "\n";
+                    MainActivity.this.runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            msg.setText(message);
+                        }
+                    });
+
+                    SocketServerReplyThread socketServerReplyThread = new SocketServerReplyThread(socket, count);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private class SocketServerReplyThread extends Thread {
+
+        private Socket hostThreadSocket;
+        int count;
+
+        public SocketServerReplyThread(Socket hostThreadSocket, int count) {
+            this.hostThreadSocket = hostThreadSocket;
+            this.count = count;
+        }
+
+        @Override
+        public void run() {
+            OutputStream outputStream;
+            String msgReply = "Hello, you are #" + count;
+
+            try {
+                outputStream = hostThreadSocket.getOutputStream();
+                PrintStream printStream = new PrintStream(outputStream);
+                printStream.print(msgReply);
+                printStream.close();
+
+                message += "replyed: " + msgReply + "\n";
+
+                MainActivity.this.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        msg.setText(message);
+                    }
+                });
+            } catch (IOException e) {
+                e.printStackTrace();
+                message += "Something Wrong!" + e.toString() + "\n";
+            }
+
+            MainActivity.this.runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    msg.setText(message);
+                }
+            });
+        }
     }
 }
