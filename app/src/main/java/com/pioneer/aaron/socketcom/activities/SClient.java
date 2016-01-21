dcba30b3e4fdca3eccf101390648b5be53b040ed
Aaron <pioneervon@gmail.com>
2016-01-21 10:26:23 +0800

    Create server and client Activity
    Signed-off-by: Aaron <pioneervon@gmail.com>

---
+++ app/src/main/java/com/pioneer/aaron/socketcom/activities/SClient.java
@@ -0,0 +1,9 @@
+package com.pioneer.aaron.socketcom.activities;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Aaron on 1/21/16.
+ */
+public class SClient extends AppCompatActivity {
+}
---
+++ app/src/main/java/com/pioneer/aaron/socketcom/activities/SServer.java
@@ -0,0 +1,10 @@
+package com.pioneer.aaron.socketcom.activities;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Aaron on 1/21/16.
+ */
+public class SServer extends AppCompatActivity {
+
+}
