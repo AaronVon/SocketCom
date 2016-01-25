f89f9c5ee72ae1656a2936110ba0457c579a8841
Aaron <pioneervon@gmail.com>
2016-01-25 10:44:37 +0800

    Server build
    Signed-off-by: Aaron <pioneervon@gmail.com>

--- app/src/main/java/com/pioneer/aaron/socketcom/activities/SClient.java
+++ app/src/main/java/com/pioneer/aaron/socketcom/activities/SClient.java
@@ -5,14 +5,24 @@ import android.support.v7.app.AppCompatActivity;
 
 import com.pioneer.aaron.socketcom.R;
 
+import butterknife.ButterKnife;
+
 /**
  * Created by Aaron on 1/21/16.
  */
 public class SClient extends AppCompatActivity {
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_sclient);
+        ButterKnife.bind(this);
+
+        loadView();
+    }
+
+    private void loadView() {
 
     }
 }
--- app/src/main/java/com/pioneer/aaron/socketcom/activities/SServer.java
+++ app/src/main/java/com/pioneer/aaron/socketcom/activities/SServer.java
@@ -2,17 +2,161 @@ package com.pioneer.aaron.socketcom.activities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 import com.pioneer.aaron.socketcom.R;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketException;
+import java.util.Enumeration;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 /**
  * Created by Aaron on 1/21/16.
  */
 public class SServer extends AppCompatActivity {
+    @Bind(R.id.info_port)
+    TextView info_port;
+    @Bind(R.id.info_ip)
+    TextView info_ip;
+    @Bind(R.id.msg)
+    TextView msg;
+
+    String message = "";
+    ServerSocket serverSocket;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_sserver);
+        ButterKnife.bind(this);
 
+        loadView();
+    }
+
+    private void loadView() {
+        info_ip.setText(getIPAddress());
+
+        Thread socketServerThread = new Thread(new SocketServerThread());
+        socketServerThread.start();
+    }
+
+    private String getIPAddress() {
+        String ip = "";
+        try {
+            Enumeration<NetworkInterface> enumeration = NetworkInterface.getNetworkInterfaces();
+            while (enumeration.hasMoreElements()) {
+                NetworkInterface networkInterface = enumeration.nextElement();
+                Enumeration<InetAddress> enumInetAddress = networkInterface.getInetAddresses();
+                while (enumInetAddress.hasMoreElements()) {
+                    InetAddress inetAddress = enumInetAddress.nextElement();
+                    if (inetAddress.isSiteLocalAddress()) {
+                        ip += "siteLocalAddress: " + inetAddress.getHostAddress() + "\n";
+                    }
+                }
+            }
+        } catch (SocketException e) {
+            e.printStackTrace();
+            ip += "Something Wrong!\n" + e.toString() + "\n";
+        }
+
+        return ip;
+    }
+
+    private class SocketServerThread extends Thread {
+        static final int SocketServerPort = 8888;
+        int count = 0;
+
+        @Override
+        public void run() {
+            try {
+                serverSocket = new ServerSocket(SocketServerPort);
+                SServer.this.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        info_port.setText("Listening: " + serverSocket.getLocalPort());
+                    }
+                });
+
+                while (true) {
+                    Socket socket = serverSocket.accept();
+                    ++count;
+                    message += "#" + count + " from " + socket.getInetAddress() + ":" + socket.getPort() + "\n";
+                    SServer.this.runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            msg.setText(message);
+                        }
+                    });
+
+                    SocketServerReplyThread socketServerReplyThread = new SocketServerReplyThread(socket, count);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private class SocketServerReplyThread extends Thread {
+
+        private Socket hostThreadSocket;
+        int count;
+
+        public SocketServerReplyThread(Socket hostThreadSocket, int count) {
+            this.hostThreadSocket = hostThreadSocket;
+            this.count = count;
+        }
+
+        @Override
+        public void run() {
+            OutputStream outputStream;
+            String msgReply = "Hello, you are #" + count;
+
+            try {
+                outputStream = hostThreadSocket.getOutputStream();
+                PrintStream printStream = new PrintStream(outputStream);
+                printStream.print(msgReply);
+                printStream.close();
+
+                message += "replyed: " + msgReply + "\n";
+
+                SServer.this.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        msg.setText(message);
+                    }
+                });
+            } catch (IOException e) {
+                e.printStackTrace();
+                message += "Something Wrong!" + e.toString() + "\n";
+            }
+
+            SServer.this.runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    msg.setText(message);
+                }
+            });
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (serverSocket != null) {
+            try {
+                serverSocket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
